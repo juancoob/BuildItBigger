8413d4452f7a4033d597de189315bf45b17f91ce
juancoob <ja.cobos.obrero@gmail.com>
2018-05-13 16:10:42 +0200

    feat: Added the Android library

---
+++ showjokes/src/main/java/com/juancoob/nanodegree/and/showjokes/ShowJokes.java
@@ -0,0 +1,40 @@
+package com.juancoob.nanodegree.and.showjokes;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class ShowJokes extends AppCompatActivity {
+
+    public static final String JOKE = "joke";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_jokes);
+        if(getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent = getIntent();
+        if(intent.hasExtra(JOKE)) {
+            TextView textView = findViewById(R.id.tv_joke);
+            textView.setText(intent.getStringExtra(JOKE));
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
