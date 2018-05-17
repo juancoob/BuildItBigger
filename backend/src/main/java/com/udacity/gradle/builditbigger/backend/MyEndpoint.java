82f890a2b2604ba5a258f7a292bfb12534067c52
juancoob <ja.cobos.obrero@gmail.com>
2018-05-17 22:41:20 +0200

    feat: Changed the endpoint to get jokes

--- backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
+++ backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
@@ -3,6 +3,7 @@ package com.udacity.gradle.builditbigger.backend;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
+import com.juancoob.nanodegree.and.funnyjavajokes.JokeTeller;
 
 import javax.inject.Named;
 
@@ -18,12 +19,13 @@ import javax.inject.Named;
 )
 public class MyEndpoint {
 
-    /** A simple endpoint method that takes a name and says Hi back */
-    @ApiMethod(name = "sayHi")
-    public MyBean sayHi(@Named("name") String name) {
-        MyBean response = new MyBean();
-        response.setData("Hi, " + name);
+    /* Endpoint which get a joke */
 
+    @ApiMethod(name = "getJoke")
+    public MyBean getJoke() {
+        MyBean response = new MyBean();
+        JokeTeller mJokeTeller = new JokeTeller();
+        response.setData(mJokeTeller.getfunnyJavaJoke());
         return response;
     }
 
