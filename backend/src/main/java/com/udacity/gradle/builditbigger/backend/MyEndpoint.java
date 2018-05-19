bfca554c9f58004d7b90c51c7dbab85d1a7be3e8
juancoob <ja.cobos.obrero@gmail.com>
2018-05-19 18:54:11 +0200

    feat: Updated the endpoint file to get jokes

--- backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
+++ backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
@@ -5,8 +5,6 @@ import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
 import com.juancoob.nanodegree.and.funnyjavajokes.JokeTeller;
 
-import javax.inject.Named;
-
 /** An endpoint class we are exposing */
 @Api(
         name = "myApi",
@@ -24,8 +22,7 @@ public class MyEndpoint {
     @ApiMethod(name = "getJoke")
     public MyBean getJoke() {
         MyBean response = new MyBean();
-        JokeTeller mJokeTeller = new JokeTeller();
-        response.setData(mJokeTeller.getfunnyJavaJoke());
+        response.setData(JokeTeller.getfunnyJavaJoke());
         return response;
     }
 
